2ddeec561912af7fbf1b0f271d2341f55e4fa610
Laiba Faisal <lfj1310@gmail.com>
2023-12-30 23:23:48 +0500

    Assignment5

---
+++ Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace _213002_Assignment05.Models
+{
+    public class Person
+    {
+        public int PersonID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
